e2b6750c2e75d6f4ac49841fabcf25b19ec75715
Sam Odulaja <samuelod@buffalo.edu>
2023-04-06 16:52:11 -0400

    Changed names & added imports to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,12 @@
 import React from 'react'
+import './App.scss';
+import BeerList from './Components/BeerList/BeerList';
+import BeerInfo from './Components/BeerInfo/BeerInfo';
+import Filter from './Components/Filter/Filter';
+import Header from './Components/Header/Header';
+import Nav from './Components/Nav/Nav';
+import SearchBar from './Components/SearchBar/SearchBar';
+
 
 const App = () => {
   return (
